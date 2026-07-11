73c1d5fba6a28dfe153c979bc3c8117f2b7f6269
Frostre_ <n9043395@gmail.com>
2026-07-11 17:01:06 +0200

    Rename namespace from cbfbeyond to clickbeyondframes

--- src/Checker.hpp
+++ src/Checker.hpp
@@ -4,7 +4,7 @@
 #include <functional>
 #include <sstream>
 
-namespace cbfbeyond::checker {
+namespace clickbeyondframes::checker {
     RuntimeState getState() {
         return {
             false,
