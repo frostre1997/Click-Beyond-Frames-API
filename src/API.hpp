941f119f72bcfb406dc1a04dc5dfc3a47beea49a
Frostre_ <n9043395@gmail.com>
2026-07-11 16:58:40 +0200

    Add API functions to check installation and version

--- src/API.hpp
+++ src/API.hpp
@@ -1,1 +1,15 @@
+#include <Geode/loader/Mod.hpp>
+#include <ClickBeyondFrames/API.hpp>
 
+namespace cbfbeyond::dev {
+    bool isInstalled() {
+        return Mod::get() != nullptr;
+    }
+
+    std::string getVersion() {
+        if (auto mod = Mod::get()) {
+            return mod->getVersion().toString();
+        }
+        return "unknown";
+    }
+}
